a774c06800aa98884a479d33f9b86fa969a45da5
jaroshevskii <jaroshevskiisasha@gmail.com>
2021-06-06 20:05:18 +0300

    Update app.cc

--- 5/app.cc
+++ 5/app.cc
@@ -1,53 +1,34 @@
 #include <iostream>
 
-void arraySorting(int (&array)[3][3], const int& columnsLenght, const int& linesLenght) {
-  int temp;
-  for (int i = 0; i < columnsLenght; ++i) {
-    for (int j = 0; j < linesLenght; ++j) {
-      for (int a = 0; a < columnsLenght; ++a) {
-        for (int b = 0; b < linesLenght; ++b) {
-          if (array[i][j] < array[a][b]) {
-            int temp = array[i][j];
-
-            array[i][j] = array[a][b];
-            array[a][b] = temp;
-          }
-        }
+void arraySorting(int* array, const int& lenght) {
+  for (int i = 0; i < lenght; ++i) {
+    for (int j = 0; j < lenght; ++j) {
+      if (array[i] < array[j]) {
+        std::swap(array[i], array[j]);
       }
     }
   }
 }
 
 int main() {
-  const int columnsLenght = 3;
-  const int linesLenght = 3;
+  const int lenght = 3;
 
-  int a[columnsLenght][linesLenght] {
-    {2, 4, 3},
-    {2, 1, 6},
-    {7, 8, 4}
-  };
+  int a[lenght] { 9, 8, 7 };
+  int b[lenght] { 6, 5, 4 };
+
+  const int cLenght = 2 * lenght;
+  int c[cLenght];
   
-  int b[columnsLenght][linesLenght] {
-    {7, 4, 3},
-    {9, 1, 6},
-    {7, 8, 4}
-  };
-
-  int c[columnsLenght + linesLenght][linesLenght];
-
-  arraySorting(c, columnsLenght + linesLenght, linesLenght);
-
-  for (int i = 0; i < columnsLenght + linesLenght; ++i) {
-    for (int j = 0; j < linesLenght; ++j) {
-      std::cout << c[i][j] << ' ';
-    }
-    std::cout << '\n';
+  for (int i = 0; i < lenght; ++i) {
+    c[i] = a[i];
+    c[i + lenght] = b[i];
   }
+  
+  arraySorting(c, cLenght);
 
-    
-
-
-
+  for (int i = 0; i < cLenght; ++i) {
+    std::cout << c[i] << ' ';
+  }
+  std::cout << '\n';
   return 0;
 }
